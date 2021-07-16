2178015096bfe160acf8d26767fb9dbbbb179470
Marcelo Martins <m.a.martins@live.com>
2021-07-15 23:03:33 -0300

    Ajustadas variáveis de ambiente

--- pages/api/communities.js
+++ pages/api/communities.js
@@ -2,7 +2,7 @@ import { SiteClient } from 'datocms-client'
 
 export default async function api (request, response) {
   if (request.method === 'POST') {
-    const apiKey = 'f694c58a5a828c10b79f27f5812e7e'
+    const apiKey = process.env.DATOCMD_API_TOKEN
     const client = new SiteClient(apiKey)
     const record = await client.items.create({
       itemType: '968403',
